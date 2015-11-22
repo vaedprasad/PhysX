a156ed70986fc5b6ff5d0a067323eedec23da900
PhysicX10 <gperiodmarketwatch@gmail.com>
2015-11-22 16:49:41 -0500

    Extra Text Removed From Return String

--- src/PhyCalc.java
+++ src/PhyCalc.java
@@ -36,9 +36,13 @@ public class PhyCalc
     {
         try{
             String v = Cal.useFunc(URLEncoder.encode("integral " + a, "UTF-8"));
-            v = v.substring(0, v.length()-8) + vel;
+            v = v.substring(a.length() + 16, v.length()-8);
+            if(!vel.equals("0"))
+                v += vel;
             String x = Cal.useFunc(URLEncoder.encode("integral " + v, "UTF-8"));
-            x = x.substring(0, x.length()-8) + pos;
+            x = x.substring(v.length() + 16, x.length()-8);
+            if(!pos.equals("0"))
+                x += pos;
             return x;
         }
         catch(Exception e)
