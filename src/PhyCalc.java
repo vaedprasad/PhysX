c6b77f7e71429e0922e05c4a4be304b2b1286c07
PhysicX10 <gperiodmarketwatch@gmail.com>
2015-11-22 10:30:05 -0500

    PhyCal made

---
+++ src/PhyCalc.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class PhyCalc here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PhyCalc
+{
+    public static double forceToAcc(double F, double m)
+    {
+        return F/m;
+    }
+    public static double impulseToVel(double I, double m)
+    {
+        return I/m;
+    }
+}
